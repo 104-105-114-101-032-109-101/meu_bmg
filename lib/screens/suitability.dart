1e0902cc73783ec751eacbbc2f44fb20a264976e
Nicholas <nicholas@hopf.pt>
2020-05-29 11:38:37 +0100

    Suitability implemented, lacking db

--- lib/screens/suitability.dart
+++ lib/screens/suitability.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
+import 'homepage.dart';
 
 class Suitability extends StatefulWidget {
   @override
@@ -9,80 +10,98 @@ class Suitability extends StatefulWidget {
 
 class _SuitabilityState extends State<Suitability> {
   int _radioValue = -1;
+  int _questionNumber = 0;
+  List <int> _radioValues = [];
 
-  void _handleRadioValueChange1(int value) {
-    setState(() {
-      print(value);
-      _radioValue = value;
-      switch (_radioValue) {
-        case 0:
-          Fluttertoast.showToast(msg: 'Correct !',toastLength: Toast.LENGTH_SHORT);
-        break;
-        case 1:
-          Fluttertoast.showToast(msg: 'Try again !',toastLength: Toast.LENGTH_SHORT);
-        break;
-        case 2:
-          Fluttertoast.showToast(msg: 'Try again !',toastLength: Toast.LENGTH_SHORT);
-        break;
-      }
-    });
-  }
+  void _handleRadioValueChange(int value) {setState(() {_radioValue = value;});}
 
   @override
   Widget build(BuildContext context) {
-    List<String> content = ["Menos que a inflação","Mais que a inflação","Continua sendo roubo do mesmo jeito. Este texto é longo de propósito, o Estado devia cair, imposto é roubo"];
+    List<List<String>> content = [["Qual a sua faixa etária?", "18-23 anos", "24-29 anos", "30-35 anos", "35+ anos"],
+                                  ["Atualmente, você tem fonte de renda própria?", "Sim", "Não"],
+                                  ["Seus hábitos de controle de gastos são:", "Bem planejados. Controlo e conheço todos os meus gastos", "Organizados na maior parte do tempo, mas às vezes fujo do planejamento", "Não tenho nenhum método para controlar e planejar minhas despesas"],
+                                  ["Suponha que você tem R\$100 investidos num fundo, rendendo 2% ao ano. Depois de 5 anos, quanto dinheiro você terá neste fundo?", "Mais que R\$102", "Exatamente R\$102", "Menos que R\$102", "Não sei"],
+                                  ["Imagine que a sua poupança rende 1% ao ano e a inflação anual é de 2%. Daqui a um ano, o dinheiro que você tem na conta vai valer:", "Mais do que vale hoje", "O mesmo que vale hoje", "Menos do que vale hoje", "Não sei"],
+                                  ["Quando há uma redução da taxa Selic, os rendimentos da poupança e do tesouro direto:", "Aumentam", "Diminuem", "Não sei"],
+                                  ["Um produto parcelado em 15 meses tem prestações mais caras do que um produto parcelado em 30 meses, mas a taxa de juros costuma ser menor quando há menos parcelas.", "Verdadeiro", "Falso", "Não sei"],
+                                  ["Suponha que você tem uma dívida de R\$1.000 com taxa de juros composta de 20% ao ano. Se você não pagar a dívida, quantos anos serão necessários para que sua dívida duplique?", "Menos de 2 anos", "De 2 a 4 anos", "De 5 a 9 anos", "Não sei"],];
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return
       SafeArea(
         child: new MaterialApp(
           home: new Scaffold(
-            appBar: AppBar(
-              title: new Text('Teste seus conhecimentos'),
-              centerTitle: true,
-              backgroundColor: const Color(0xffff8f00),
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(screenHeight * 0.06),
+              child: AppBar(
+                title: Column(
+                  children: <Widget>[ Transform.translate(
+                      offset: Offset(0.0, -screenHeight * 0.02),
+                      child: new Text(
+                        'Teste seus conhecimentos',
+                        textAlign: TextAlign.center,
+                        style: new TextStyle( fontSize: 20.0, )
+                      )
+                    )
+                  ],
+                ),
+                centerTitle: true,
+                backgroundColor: const Color(0xffff8f00),
+              ),
             ),
             body: new Container(
-              padding: EdgeInsets.all(8.0),
               child: new Column(
-                children: <Widget>[
-                  new Padding( padding: new EdgeInsets.all(18.0) ),
-                  new Text(
-                    'Quanto rende o FGTS?',
-                    style: new TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                  new Padding( padding: new EdgeInsets.only(top: 25.0, bottom: 18.0) ),
+                  new Container (
+                    width: screenWidth*0.65,
+                    child: new Text(
+                      content[_questionNumber][0],
+                      textAlign: TextAlign.left,
+                      style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
-                  SizedBox(height: 30),
-                  for ( int i=0; i<content.length; i++ )
+                  SizedBox(height: 45),
+                  for ( int i=1; i<content[_questionNumber].length; i++ )
                     new Row(
                       children: <Widget>[
                         new Padding(
                           padding: new EdgeInsets.all(18.0),
                         ),
                         new Radio(
+                          activeColor: const Color(0xffff8f00),
                           value: i,
                           groupValue: _radioValue,
-                          onChanged: _handleRadioValueChange1,
+                          onChanged: _handleRadioValueChange,
                         ),
                         new Container (
-                          width: MediaQuery.of(context).size.width*0.65,
-                          child: new Text (content[i], textAlign: TextAlign.left),
+                          width: screenWidth*0.65,
+                          child: new Text (content[_questionNumber][i], textAlign: TextAlign.left),
                         )
                       ],
                     ),
-
-                  Transform.translate(
-                    // TODO: change to querysize minus x or to percentage of it
-                    offset: Offset(130.0, 180.0),
-                    child: Ink(
-                      decoration: const ShapeDecoration(
-                      color: const Color(0xffff8f00),
-                      shape: CircleBorder(),
-                    ),
-                      child: IconButton(
-                        icon: Icon(Icons.arrow_forward_ios),
-                        color: Colors.white,
-                        onPressed: () {print("Hello");},
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child:
+                      Transform.translate(
+                        offset: Offset(-12, -12),
+                        child:
+                        Ink(
+                          decoration: const ShapeDecoration(
+                            color: const Color(0xffff8f00),
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.arrow_forward_ios),
+                            color: Colors.white,
+                            onPressed: () {nextQuestion(content.length);},
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -94,18 +113,21 @@ class _SuitabilityState extends State<Suitability> {
       );
   }
 
-  void resetSelection() {
-    _handleRadioValueChange1(-1);
-  }
-
-  void validateAnswers() {
+  void nextQuestion(lastQuestionIndex) {
     if (_radioValue == -1) {
-      Fluttertoast.showToast(msg: 'Please select atleast one answer',
+      Fluttertoast.showToast(msg: 'Selecione uma resposta',
       toastLength: Toast.LENGTH_SHORT);
+    } else {
+      _radioValues.add(_radioValue);
+      _questionNumber += 1;
+      if (_questionNumber == lastQuestionIndex) {
+        print(_radioValues); // TODO: Send values to database
+        Navigator.push(
+          context, MaterialPageRoute(builder: (context) => PginaInicial1()),
+        );
       } else {
-        Fluttertoast.showToast(
-          msg: 'Your total score is: 3 out of 5',
-          toastLength: Toast.LENGTH_LONG);
+        setState(() { _radioValue = -1; });
       }
+    }
   }
 }
